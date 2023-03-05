18c5a4aff590595cf7ed44dc6a06b5ec9e247c5f
Umur Gokalp <ugokalp@Umurs-MBP-M1-2.home>
2023-03-05 19:46:45 +0300

    fix: correctly type queryBuilder

--- src/config/db/queryBuilder.ts
+++ src/config/db/queryBuilder.ts
@@ -1,4 +1,4 @@
-import { Query } from "rado/index"
+import { QueryData } from "rado/index"
 import { SqliteFormatter } from "rado/sqlite"
 
 type Param = {
@@ -6,11 +6,15 @@ type Param = {
   value: string
 }
 
+interface Model {
+  toJSON: () => QueryData
+}
+
 /* 
   Query builder is used to convert a rado query to a sql statement and an array of parameters.
 */
 
-const queryBuilder = <T>(model: Query<T>) => {
+const queryBuilder = (model: Model) => {
   const formatter = new SqliteFormatter()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
   const statement = formatter.compile(model.toJSON())
