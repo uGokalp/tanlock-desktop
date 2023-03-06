64061b065b2794323b0a232575d8db4e3e1e4cd4
Umur Gokalp <ugokalp@Umurs-MBP-M1-2.home>
2023-03-07 00:01:19 +0300

    feat: upgraded to a reliable sqlite provider

--- src/config/db/connector.ts
+++ src/config/db/connector.ts
@@ -1,6 +1,7 @@
 import { Statement } from "rado/index"
 
 import { invoke, resourcePath } from "@/utils/tauri"
+import Database from "tauri-plugin-sql-api"
 
 type Result = {
   result: string
@@ -24,37 +25,17 @@ class RadoDb {
     this.path = "./dev.db"
   }
   async select<T>(sql: Statement["sql"], values?: string[]): Promise<T[]> {
-    const path = await resourcePath(this.path)
-    const data = invoke("plugin:sqlite|select", {
-      path,
-      sql,
-      values: values ?? [],
-    })
-    try {
-      const res = this.parse<T>((await data) as Result[])
-      return res
-    } catch (e) {
-      console.log(e)
-      await this.open()
-      const res = this.parse<T>((await data) as Result[])
-      return res
-    }
+    const db = await Database.load("sqlite:dev.db")
+    const data = await db.select(sql, values)
+    const res = this.parse<T>((await data) as Result[])
+    return res
   }
 
   async execute(sql: Statement["sql"], values?: string[]): Promise<boolean> {
-    const path = await resourcePath(this.path)
-    const exec = values
-      ? invoke("plugin:sqlite|execute2", { path, sql, values })
-      : invoke("plugin:sqlite|execute", { path, sql })
-
-    try {
-      const res = await exec
-      return res as Promise<boolean>
-    } catch (e) {
-      await this.open()
-      const res = await exec
-      return res as Promise<boolean>
-    }
+    const db = await Database.load("sqlie:dev.db")
+    const exec = db.execute(sql, values)
+    const res = await exec
+    return Boolean(res.rowsAffected)
   }
 
   async open() {
